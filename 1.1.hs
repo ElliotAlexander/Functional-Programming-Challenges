8d171eb10e9eea57d28ccecd8ca31adf53efa19e
Elliot Alexander <epurvis97@gmail.com>
2017-12-06 19:04:58 +0000

    Q2 still needs a bit of work

--- 1.1.hs
+++ 1.1.hs
@@ -5,10 +5,18 @@ data Expr = App Expr Expr | Lam Int Expr | Var Int deriving (Show, Eq)
 
 -- Exercise 1
 
-freesub :: Expr -> [Int]
+freeVariables :: Expr -> [Int]
 -- Continue computation minus input lambda-defined variables. 
-freesub (Lam x e1) = freesub e1 \\ [x]
+freeVariables (Lam x e1) = freeVariables e1 \\ [x]
 -- Has to be a pre-set var. 
-freesub (Var x) = [x]
+freeVariables (Var x) = [x]
 -- recursive combine each sub-expressions output. 
-freesub (App e1 e2) = (freesub e1) ++ (freesub e2)
+freeVariables (App e1 e2) = (freeVariables e1) ++ (freeVariables e2)
+
+-- Exercise 2
+rename :: Expr -> Int -> Int -> Expr
+rename (Lam x e1) v1 v2 | x == v1 = Lam (v2) (rename e1 v1 v2) | otherwise = Lam (x) (rename e1 v1 v2)
+rename (Var x) v1 v2 | x == v1 = Var v2 | otherwise = Var x
+rename (App e1 e2) v1 v2 = App (rename e1 v1 v2) (rename e2 v1 v2)
+
+-- Exercise 3
