af988fd21d19a609f19b9b0f324788030888f961
ElliotAlexander <epurvis97@gmail.com>
2017-12-06 22:46:01 +0000

    I think two is fixed. Bit of a dirty solution but uh

--- 1.1.hs
+++ 1.1.hs
@@ -15,10 +15,11 @@ freeVariables (App e1 e2) = (freeVariables e1) ++ (freeVariables e2)
 -- not fully functional - needs work. 
 
 rename :: Expr -> Int -> Int -> Expr
-rename (Lam x e1) v1 v2 | x == v1 = Lam (v2) (rename e1 v1 v2) | otherwise = Lam (x) (rename e1 v1 v2)
+rename (Lam x e1) v1 v2 | x == v1 = Lam (v2) (rename e1 0 0) | otherwise = Lam (x) (rename e1 v1 v2)
 rename (Var x) v1 v2 | x == v1 = Var v2 | otherwise = Var x
 rename (App e1 e2) v1 v2 = App (rename e1 v1 v2) (rename e2 v1 v2)
 
+{-|
 -- Exercise 3
 alphaEquivalent :: Expr -> Expr -> Bool
 alphaEquivalent (Lam x e1) = 
@@ -31,3 +32,5 @@ alphaEquivalent' :: Expr -> Int -> Int -> Expr
 alphaEquivalent' (Lam x e1) v1 v2 | x == v1 = Lam (v2) (rename e1 v1 v2) | otherwise = Lam (x) (rename e1 v1 v2)
 alphaEquivalent' (Var x) v1 v2 | x == v1 = Var v2 | otherwise = Var x
 alphaEquivalent' (App e1 e2) v1 v2 = App (rename e1 v1 v2) (rename e2 v1 v2)
+
+|-}
