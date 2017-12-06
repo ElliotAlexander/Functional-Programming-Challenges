0555d0cda7fe3a9cf54828b55bb22ab94b6634f8
ElliotAlexander <epurvis97@gmail.com>
2017-12-06 23:42:04 +0000

    I like to think ex4 is working. made little progress on 3.

--- 1.1.hs
+++ 1.1.hs
@@ -1,5 +1,5 @@
-import Data.List
-
+import qualified Data.List as List
+import qualified Data.Set as Set
 
 data Expr = App Expr Expr | Lam Int Expr | Var Int deriving (Show, Eq)
 
@@ -7,7 +7,7 @@ data Expr = App Expr Expr | Lam Int Expr | Var Int deriving (Show, Eq)
 -- Seems to work well.
 
 freeVariables :: Expr -> [Int]
-freeVariables (Lam x e1) = freeVariables e1 \\ [x]
+freeVariables (Lam x e1) = freeVariables e1 List.\\ [x]
 freeVariables (Var x) = [x]
 freeVariables (App e1 e2) = (freeVariables e1) ++ (freeVariables e2)
 
@@ -15,22 +15,27 @@ freeVariables (App e1 e2) = (freeVariables e1) ++ (freeVariables e2)
 -- not fully functional - needs work. 
 
 rename :: Expr -> Int -> Int -> Expr
-rename (Lam x e1) v1 v2 | (x == v1) && (elem x (freeVariables e1)) = Lam (v2) (rename e1 v1 v2) | otherwise = Lam (x) (rename e1 v1 v2)
+rename (Lam x e1) v1 v2 | x == v1 && (elem x (freeVariables e1)) = Lam (v2) (rename e1 v1 v2) | otherwise = Lam (x) (rename e1 v1 v2)
 rename (Var x) v1 v2 | x == v1 = Var v2 | otherwise = Var x
 rename (App e1 e2) v1 v2 = App (rename e1 v1 v2) (rename e2 v1 v2)
 
-{-|
+
 -- Exercise 3
-alphaEquivalent :: Expr -> Expr -> Bool
-alphaEquivalent (Lam x e1) = 
+
+--alphaEquivalent :: Expr -> Expr -> Bool
+--alphaEquivalent e1 e2 = all (\x -> x elem (alphaEquivalent' e1 -1)) (alphaEquivalent' e2 -1)
+--alphaEquivalent e1 e2 =  (alphaEquivalent' e1 10) Set.isSubsetOf (alphaEquivalent' e2 -10)
 
 
--- If we rename all variables to a constant value, then compare strings?
--- We need to fix problems with 2 first though...
 
-alphaEquivalent' :: Expr -> Int -> Int -> Expr
-alphaEquivalent' (Lam x e1) v1 v2 | x == v1 = Lam (v2) (rename e1 v1 v2) | otherwise = Lam (x) (rename e1 v1 v2)
-alphaEquivalent' (Var x) v1 v2 | x == v1 = Var v2 | otherwise = Var x
-alphaEquivalent' (App e1 e2) v1 v2 = App (rename e1 v1 v2) (rename e2 v1 v2)
+-- Exercise 4
 
-|-}
+hasRedex :: Expr -> Bool
+hasRedex (Lam x e1) = hasRedex' e1
+hasRedex (Var x) = False
+hasRedex (App e1 e2) = hasRedex' (App e1 e2)
+
+hasRedex' :: Expr -> Bool
+hasRedex' (Lam x e1) = True
+hasRedex' (App e1 e2) | (hasRedex' e1 == False && hasRedex' e2 == False) = False | otherwise = True
+hasRedex' (Var x) = False
