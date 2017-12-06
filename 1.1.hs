b4ab7af3ece88b7247c2172d9dfabf76971b80a9
ElliotAlexander <epurvis97@gmail.com>
2017-12-06 22:39:11 +0000

    think i've a nice solution for 3

--- 1.1.hs
+++ 1.1.hs
@@ -4,16 +4,16 @@ import Data.List
 data Expr = App Expr Expr | Lam Int Expr | Var Int deriving (Show, Eq)
 
 -- Exercise 1
+-- Seems to work well.
 
 freeVariables :: Expr -> [Int]
--- Continue computation minus input lambda-defined variables. 
 freeVariables (Lam x e1) = freeVariables e1 \\ [x]
--- Has to be a pre-set var. 
 freeVariables (Var x) = [x]
--- recursive combine each sub-expressions output. 
 freeVariables (App e1 e2) = (freeVariables e1) ++ (freeVariables e2)
 
 -- Exercise 2
+-- not fully functional - needs work. 
+
 rename :: Expr -> Int -> Int -> Expr
 rename (Lam x e1) v1 v2 | x == v1 = Lam (v2) (rename e1 v1 v2) | otherwise = Lam (x) (rename e1 v1 v2)
 rename (Var x) v1 v2 | x == v1 = Var v2 | otherwise = Var x
@@ -21,3 +21,13 @@ rename (App e1 e2) v1 v2 = App (rename e1 v1 v2) (rename e2 v1 v2)
 
 -- Exercise 3
 alphaEquivalent :: Expr -> Expr -> Bool
+alphaEquivalent (Lam x e1) = 
+
+
+-- If we rename all variables to a constant value, then compare strings?
+-- We need to fix problems with 2 first though...
+
+alphaEquivalent' :: Expr -> Int -> Int -> Expr
+alphaEquivalent' (Lam x e1) v1 v2 | x == v1 = Lam (v2) (rename e1 v1 v2) | otherwise = Lam (x) (rename e1 v1 v2)
+alphaEquivalent' (Var x) v1 v2 | x == v1 = Var v2 | otherwise = Var x
+alphaEquivalent' (App e1 e2) v1 v2 = App (rename e1 v1 v2) (rename e2 v1 v2)
