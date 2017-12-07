faa587c2a5b2c321b723992fe6a4292380e85006
ElliotAlexander <epurvis97@gmail.com>
2017-12-07 03:45:54 +0000

    Removed 1.5.hs, moved ex5 back into main file

--- 1.1.hs
+++ 1.1.hs
@@ -43,6 +43,6 @@ hasRedex' (Var x) = False
 
 -- Exercise 5
 substitute :: Expr -> Int -> Expr -> Expr
-substitute (Lam x e1) i e2 | x == i = (Lam e2 substitute(e1)) | otherwise = (Lam x (substitute(e1)))
-substitute (App e1 e2) i e3 = (App (substitute e1) (substitute e2))
+substitute (Lam x e1) i e2 | x == i = (App e2 (substitute e1 i e2)) | otherwise = (Lam x (substitute e1 i e2))
+substitute (App e1 e2) i e3 = (App (substitute e1 i e3) (substitute e2 i e3))
 substitute (Var x) i e1 | x == i = e1 | otherwise = (Var x)
