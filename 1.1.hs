6424f74607104486924963972c6bf3d32145fb10
ElliotAlexander <epurvis97@gmail.com>
2017-12-07 03:56:26 +0000

    Ex5 seems to work? Needs more testing. I'll leave it until the morning.

--- 1.1.hs
+++ 1.1.hs
@@ -46,3 +46,7 @@ substitute :: Expr -> Int -> Expr -> Expr
 substitute (Lam x e1) i e2 | x == i = (App e2 (substitute e1 i e2)) | otherwise = (Lam x (substitute e1 i e2))
 substitute (App e1 e2) i e3 = (App (substitute e1 i e3) (substitute e2 i e3))
 substitute (Var x) i e1 | x == i = e1 | otherwise = (Var x)
+
+
+
+-- Pretty Printer :)
