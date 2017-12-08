fb776e96be22c3c2f1f94c27971135ed170e375f
ElliotAlexander <epurvis97@gmail.com>
2017-12-08 08:21:25 +0000

    Added lambda symbol

--- 1.1.hs
+++ 1.1.hs
@@ -51,8 +51,8 @@ substitute (Var x) i e1 | x == i = e1 | otherwise = (Var x)
 
 -- Pretty Printer :)
 prettyPrint :: Expr -> String
-prettyPrint (Lam x (Lam a e2)) = "\\x" ++ show x ++ (prettyPrint' (Lam a e2))
-prettyPrint (Lam x e1) = "\\x" ++ show x ++ "->" ++ (prettyPrint e1)
+prettyPrint (Lam x (Lam a e2)) = "λx" ++ show x ++ (prettyPrint' (Lam a e2))
+prettyPrint (Lam x e1) = "λx" ++ show x ++ "->" ++ (prettyPrint e1)
 prettyPrint (App e1 e2) = (prettyPrint e1) ++ (prettyPrint e2)
 prettyPrint (Var x) = "x" ++ show x
 
